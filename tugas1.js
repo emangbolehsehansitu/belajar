a005806dc54b1a2b1a084de5161ff0aab1a0c7d2
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2023-08-11 12:29:57 +0700

    Update tugas1.js

--- tugas1.js
+++ tugas1.js
@@ -8,19 +8,19 @@
 //  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
 //   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
 
-//let word = 'JavaScript';
-//let second = 'is';
-//let third = 'awesome';
-//let fourth = 'and';
-//let fifth = 'I';
-//let sixth = 'love';
-//let seventh = 'it!';
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
 
 //code here
-//const penggabunganKata = word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh;
+const penggabunganKata = word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh;
 
 // ---------------------------------------
-//console.log(penggabunganKata);
+console.log(penggabunganKata);
 // ---------------------------------------
 
 // 2. Index Accessing - 1 by 1
@@ -33,42 +33,42 @@
 // Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
 //  tersebut!
 
- //let word = 'wow JavaScript is so cool';
- //let exampleFirstWord = word[0] + word[1] + word[2];
+ let word1 = 'wow JavaScript is so cool';
+ let exampleFirstWord = word1[0] + word1[1] + word1[2];
 
-//console.log('First Word: ' + exampleFirstWord);
+console.log('First Word: ' + exampleFirstWord);
 
-//const JavaScript =  word[3] + word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
-//const is = word[15] + word[16]
-//const so = word[18] + word[19]
-//const cool = word[21] + word[22] + word[23] + word[24]
+const JavaScript =  word[3] + word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
+const is = word[15] + word[16]
+const so = word[18] + word[19]
+const cool = word[21] + word[22] + word[23] + word[24]
 
 // ---------------------------------------
-//console.log(JavaScript);
-//console.log(is);
-//console.log(so);
-//console.log(cool);
+console.log(JavaScript);
+console.log(is);
+console.log(so);
+console.log(cool);
 // ---------------------------------------
 
 // 3. Breaking Sentence (Again) using Substring
 // Problem
 // Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
 
-//let word3 = 'wow JavaScript is so cool';
-//let exampleFirstWord3 = word3.substring(0, 3);
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
 
-//console.log('First Word: ' + exampleFirstWord3);
+console.log('First Word: ' + exampleFirstWord3);
 
-//const JavaScript3 = word3.substring(4, 15);
-//const is3 = word3.substring(15, 17);
-//const so3 = word3.substring(18, 20);
-//const cool3 = word3.substring(20, 25);
+const JavaScript3 = word3.substring(4, 15);
+const is3 = word3.substring(15, 17);
+const so3 = word3.substring(18, 20);
+const cool3 = word3.substring(20, 25);
 
 // ---------------------------------------
-//console.log(JavaScript3);
-//console.log(is3);
-//console.log(so3);
-//console.log(cool3);
+console.log(JavaScript3);
+console.log(is3);
+console.log(so3);
+console.log(cool3);
 // ---------------------------------------
 // 4. Breaking Sentence (yet Again) and Count Each Length
 // Problem
